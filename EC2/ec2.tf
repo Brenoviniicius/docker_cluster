b5ec1b7dbf0f007315b6c4a84f1bb75a0afdc23a
Breno Vínicius <brenovmsilva82@gmail.com>
2024-11-09 11:51:00 -0300

    Configuring SSH parameters

--- EC2/ec2.tf
+++ EC2/ec2.tf
@@ -2,11 +2,20 @@ resource "aws_instance" "swarm" {
   count         = var.instance_number
   ami           = var.instance_ami
   instance_type = var.instance_type
-  
-  tags          = merge(
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  key_name = "public"
+
+  tags = merge(
     local.common_tags, {
-        Project = "Cluster Swarm"
-        Name = ("cluster-${count.index + 1}")
+      Project = "Cluster Swarm"
+      Name    = ("cluster-${count.index + 1}")
     }
   )
 }
+
+resource "aws_key_pair" "public_ssh" {
+  key_name   = "public"
+  public_key = var.instance_public_ssh
+}
+
+
