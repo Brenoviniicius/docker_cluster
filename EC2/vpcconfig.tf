ede06bd4aa6fa10a6be40b77075b16a040a40a3e
Breno Vínicius <brenovmsilva82@gmail.com>
2024-11-09 11:53:30 -0300

    Customizing the VPC

---
+++ EC2/vpcconfig.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow ssh connection"
+  description = "allow shh and http"
+
+  ingress {
+    description = "allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "allow http"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
