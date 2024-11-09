f0f2c953852535b4f3e128d69767136a8f00b2a4
Breno Vínicius <brenovmsilva82@gmail.com>
2024-11-09 17:35:31 -0300

    Revert "Configuring SSH parameters"
    This reverts commit b5ec1b7dbf0f007315b6c4a84f1bb75a0afdc23a.

--- EC2/ec2.tf
+++ EC2/ec2.tf
@@ -2,20 +2,11 @@ resource "aws_instance" "swarm" {
   count         = var.instance_number
   ami           = var.instance_ami
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-  key_name = "public"
-
-  tags = merge(
+  
+  tags          = merge(
     local.common_tags, {
-      Project = "Cluster Swarm"
-      Name    = ("cluster-${count.index + 1}")
+        Project = "Cluster Swarm"
+        Name = ("cluster-${count.index + 1}")
     }
   )
 }
-
-resource "aws_key_pair" "public_ssh" {
-  key_name   = "public"
-  public_key = var.instance_public_ssh
-}
-
-
